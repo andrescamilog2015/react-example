9efd783e9eb4fc27608ebdb0e83b1d62d3f21749
Camilo Giraldo <andrescamilog2015@gmail.com>
2026-04-02 19:11:59 -0500

    feat: add another person to the list

--- index.mjs
+++ index.mjs
@@ -2,12 +2,13 @@ import { MASCULINO, FEMENINO, NO_DEFINIDO, saludar } from "./lib.mjs"
 
 function main() {
     const personas = [
-        {nombre: "camilo", sexo: MASCULINO},
-        {nombre: "luis", sexo: MASCULINO}
+        {nombre: "camilo", sexo: MASCULINO, nota: 10},
+        {nombre: "luis", sexo: MASCULINO, nota: 10},
+        {nombre: "esmeralda", sexo: FEMENINO, nota: 10}
     ]
     
     for(let persona of personas) {
-        saludar(persona.nombre, persona.sexo)
+        saludar(persona.nombre, persona.sexo)   
     }
 }
 
